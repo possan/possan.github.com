0dad20baa7a180e7dec440506f86895b4dd7fca0
Per-Olov Jernberg <possan@possan.se>
2010-12-16 17:31:32 +0100

    Updated imagescaler2

--- imagescaler2/imagescaler.js
+++ imagescaler2/imagescaler.js
@@ -74,63 +74,93 @@ ImageScaler2 = {
 
     calcRegion3: function (iw, ih, ow, oh, rx1, ry1, rx2, ry2, fill) {
 
-        var mag = ImageScaler2.calcMag(iw, ih, ow, oh, fill);
-
         var rw = rx2 - rx1;
         var rh = ry2 - ry1;
-        var rmag = ImageScaler2.calcMag(iw, ih, rw, rh, fill) * mag;
 
-        // region on input
-        var rxi = (rx1 + rx2) / 2;
-        var ryi = (ry1 + ry2) / 2;
+        var oa = ow / oh;
 
-        // region on outupt
-        var rxo = rxi * mag;
-        var ryo = ryi * mag;
+        // fix region aspect 
+        if (ow > oh) {
+            rh = rw / oa;
+        } else if (ow < oh) {
+            rw = rh * oa;
+        }
 
-        // input span
-        var rxsi = iw - rw;
-        var rysi = ih - rh;
+        // re center region
+        var rx = (rx1 + rx2) / 2;
+        var ry = (ry1 + ry2) / 2;
+        rx1 = rx - rw / 2;
+        ry1 = ry - rh / 2;
+        rx2 = rx + rw / 2;
+        ry2 = ry + rh / 2;
 
-        // output span
-        var rxso = rxsi * mag;
-        var ryso = rysi * mag;
+        var mag = ImageScaler2.calcMag(iw, ih, ow, oh, fill);
 
-        // offset percent
-        var rxp = (rxso > 0) ? rx1 / rxsi : 0;
-        var ryp = (ryso > 0) ? ry1 / rysi : 0;
+        var rmag = ImageScaler2.calcMag(rw, rh, ow, oh, fill);
+        if (mag < rmag)
+            mag = rmag;
+             
+        var how = ow * mag;
+        var hoh = oh * mag;
+        var how2 = how / 2;
+        var hoh2 = hoh / 2;
+
+        // output span (screenspace)
+
+        // input span (texturespace)
+        var rxsi = (iw * mag) - (ow);
+        var rysi = (ih * mag) - (oh); // ryso / mag;
+
+        var rxso = rxsi; // / mag;
+        var ryso = rysi; // / mag;
+
+        // offset (percent)
+        var rxp = (rxsi > 0) ? rx1 * mag / rxsi : 0;
+        var ryp = (rysi > 0) ? ry1 * mag / rysi : 0;
+
+        // clip and round
+        rxp = Math.max(0, Math.min(1, rxp));
+        ryp = Math.max(0, Math.min(1, ryp));
+        rxp = Math.round(rxp * 1000) / 1000;
+        ryp = Math.round(ryp * 1000) / 1000;
 
         // return structure
         var r = {
-            mag: mag,
             rw: rw,
             rh: rh,
+            mag: mag,
             rmag: rmag,
-            rxo: rxo,
-            ryo: ryo,
-            rxi: rxi,
-            ryi: ryi,
+            how: how,
+            hoh: hoh,
+            rw: rw,
+            rh: rh,
+            rxp: rxp,
+            ryp: ryp,
             rxso: rxso,
             ryso: ryso,
             rxsi: rxsi,
-            rysi: rysi,
-            ryp: ryp,
-            rxp: rxp
+            rysi: rysi
         };
 
-        // output size
-        var how2 = iw * mag / 2;
-        var hoh2 = ih * mag / 2;
+        // offset in screenspace
+        var cx = rxso * rxp;
+        var cy = ryso * ryp;
 
-        // output center
-        var cx = (ow / 2) - (rxso * (rxp - 0.5));
-        var cy = (oh / 2) - (ryso * (ryp - 0.5));
+        // limit region to image
+        if (fill) {
+            cx = Math.max(cx, 0);
+            cy = Math.max(cy, 0);
+            cx = Math.min(cx, rxso);
+            cy = Math.min(cy, ryso);
+        }
 
         // borders
-        r.l = Math.floor(cx - how2);
-        r.t = Math.floor(cy - hoh2);
-        r.r = Math.ceil(cx + how2);
-        r.b = Math.ceil(cy + hoh2);
+        r.cx = cx;
+        r.cy = cy;
+        r.l = Math.floor(-cx);
+        r.t = Math.floor(-cy);
+        r.r = Math.ceil(-cx + how);
+        r.b = Math.ceil(-cy + hoh);
 
         // profit
         return r;
